ba5e2a8bf593a4fab28290ed24dd383101c3da6a
Chell-Fey99 <chell.fey99@gmail.com>
2024-11-04 13:36:30 -0500

    Got spatial audio working

--- Godot Game/GeneratedSoundBanks/wwise_ids.gd
+++ Godot Game/GeneratedSoundBanks/wwise_ids.gd
@@ -2,7 +2,13 @@ class_name AK
 
 class EVENTS:
 
-	const _dict = {}
+	const CAMPFIRE = 1931646578
+	const FACING = 54298587
+
+	const _dict = {
+		"Campfire": CAMPFIRE,
+		"Facing": FACING
+	}
 
 class STATES:
 
@@ -14,28 +20,28 @@ class SWITCHES:
 
 class GAME_PARAMETERS:
 
-	const SS_AIR_PRESENCE = 3847924954
-	const SS_AIR_SIZE = 3074696722
-	const SS_AIR_FURY = 1029930033
-	const SS_AIR_MONTH = 2648548617
 	const SS_AIR_FEAR = 1351367891
-	const SS_AIR_TIMEOFDAY = 3203397129
-	const SS_AIR_TURBULENCE = 4160247818
-	const SS_AIR_STORM = 3715662592
+	const SS_AIR_MONTH = 2648548617
 	const SS_AIR_RPM = 822163944
+	const SS_AIR_SIZE = 3074696722
+	const SS_AIR_TIMEOFDAY = 3203397129
 	const SS_AIR_FREEFALL = 3002758120
+	const SS_AIR_STORM = 3715662592
+	const SS_AIR_TURBULENCE = 4160247818
+	const SS_AIR_FURY = 1029930033
+	const SS_AIR_PRESENCE = 3847924954
 
 	const _dict = {
-		"SS_Air_Presence": SS_AIR_PRESENCE,
-		"SS_Air_Size": SS_AIR_SIZE,
-		"SS_Air_Fury": SS_AIR_FURY,
-		"SS_Air_Month": SS_AIR_MONTH,
 		"SS_Air_Fear": SS_AIR_FEAR,
-		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY,
-		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
-		"SS_Air_Storm": SS_AIR_STORM,
+		"SS_Air_Month": SS_AIR_MONTH,
 		"SS_Air_RPM": SS_AIR_RPM,
-		"SS_Air_Freefall": SS_AIR_FREEFALL
+		"SS_Air_Size": SS_AIR_SIZE,
+		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY,
+		"SS_Air_Freefall": SS_AIR_FREEFALL,
+		"SS_Air_Storm": SS_AIR_STORM,
+		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
+		"SS_Air_Fury": SS_AIR_FURY,
+		"SS_Air_Presence": SS_AIR_PRESENCE
 	}
 
 class TRIGGERS:
@@ -45,9 +51,11 @@ class TRIGGERS:
 class BANKS:
 
 	const INIT = 1355168291
+	const MAIN = 3161908922
 
 	const _dict = {
-		"Init": INIT
+		"Init": INIT,
+		"Main": MAIN
 	}
 
 class BUSSES:
@@ -64,12 +72,12 @@ class AUX_BUSSES:
 
 class AUDIO_DEVICES:
 
-	const NO_OUTPUT = 2317455096
 	const SYSTEM = 3859886410
+	const NO_OUTPUT = 2317455096
 
 	const _dict = {
-		"No_Output": NO_OUTPUT,
-		"System": SYSTEM
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
 	}
 
 class EXTERNAL_SOURCES:
