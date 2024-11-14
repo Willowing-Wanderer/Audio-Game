279c9ecfc316d5b33c730e5c654835436d6788d6
Chell-Fey99 <chell.fey99@gmail.com>
2024-11-14 12:52:37 -0500

    Fixed spatial audio not working

--- Godot Game/CampfireEvent.gd
+++ Godot Game/CampfireEvent.gd
@@ -4,6 +4,7 @@ extends AkEvent3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Wwise.register_game_obj(self, self.get_name())
+	Wwise.set_3d_position(self, get_global_transform())
 	Wwise.post_event_id(AK.EVENTS.CAMPFIRE, self)
 
 
