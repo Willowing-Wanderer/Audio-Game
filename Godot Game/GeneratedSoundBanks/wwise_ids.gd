465175896efb32b743b45e0b7492c7cc132dc741
Chell-Fey99 <chell.fey99@gmail.com>
2024-11-19 22:12:49 -0500

    Added black wind and drone

--- Godot Game/GeneratedSoundBanks/wwise_ids.gd
+++ Godot Game/GeneratedSoundBanks/wwise_ids.gd
@@ -2,24 +2,34 @@ class_name AK
 
 class EVENTS:
 
-	const FIRE_HIGH = 2673322736
 	const FACING = 54298587
+	const FIRE_LOW = 771468442
+	const FACING_STICK = 2793518390
+	const FIRE_MEDIUM = 1873888577
+	const FIRE_HIGH = 2673322736
 	const FIRE_DIM = 135198910
 	const FIRE_WHOOSH = 3524664550
-	const FACING_STICK = 2793518390
 	const PICKUP = 3978245845
-	const FIRE_MEDIUM = 1873888577
-	const FIRE_LOW = 771468442
+	const TINDER = 2077643863
+	const KINDLING = 3324137807
+	const BLACK_WIND = 4020366015
+	const FUEL = 2661514705
+	const DRUIDS_MESSAGE = 518818430
 
 	const _dict = {
-		"Fire_High": FIRE_HIGH,
 		"Facing": FACING,
+		"Fire_Low": FIRE_LOW,
+		"Facing_Stick": FACING_STICK,
+		"Fire_Medium": FIRE_MEDIUM,
+		"Fire_High": FIRE_HIGH,
 		"Fire_Dim": FIRE_DIM,
 		"Fire_Whoosh": FIRE_WHOOSH,
-		"Facing_Stick": FACING_STICK,
 		"Pickup": PICKUP,
-		"Fire_Medium": FIRE_MEDIUM,
-		"Fire_Low": FIRE_LOW
+		"Tinder": TINDER,
+		"Kindling": KINDLING,
+		"Black_Wind": BLACK_WIND,
+		"Fuel": FUEL,
+		"Druids_Message": DRUIDS_MESSAGE
 	}
 
 class STATES:
@@ -32,30 +42,30 @@ class SWITCHES:
 
 class GAME_PARAMETERS:
 
-	const SS_AIR_FURY = 1029930033
 	const SS_AIR_SIZE = 3074696722
-	const SS_AIR_STORM = 3715662592
-	const SS_AIR_TURBULENCE = 4160247818
-	const SS_AIR_FREEFALL = 3002758120
-	const SS_AIR_RPM = 822163944
 	const SS_AIR_TIMEOFDAY = 3203397129
-	const PLAYER_CONE = 2039612124
 	const SS_AIR_PRESENCE = 3847924954
-	const SS_AIR_FEAR = 1351367891
+	const SS_AIR_TURBULENCE = 4160247818
+	const SS_AIR_RPM = 822163944
+	const SS_AIR_STORM = 3715662592
+	const SS_AIR_FURY = 1029930033
+	const SS_AIR_FREEFALL = 3002758120
 	const SS_AIR_MONTH = 2648548617
+	const PLAYER_CONE = 2039612124
+	const SS_AIR_FEAR = 1351367891
 
 	const _dict = {
-		"SS_Air_Fury": SS_AIR_FURY,
 		"SS_Air_Size": SS_AIR_SIZE,
-		"SS_Air_Storm": SS_AIR_STORM,
-		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
-		"SS_Air_Freefall": SS_AIR_FREEFALL,
-		"SS_Air_RPM": SS_AIR_RPM,
 		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY,
-		"Player_Cone": PLAYER_CONE,
 		"SS_Air_Presence": SS_AIR_PRESENCE,
-		"SS_Air_Fear": SS_AIR_FEAR,
-		"SS_Air_Month": SS_AIR_MONTH
+		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
+		"SS_Air_RPM": SS_AIR_RPM,
+		"SS_Air_Storm": SS_AIR_STORM,
+		"SS_Air_Fury": SS_AIR_FURY,
+		"SS_Air_Freefall": SS_AIR_FREEFALL,
+		"SS_Air_Month": SS_AIR_MONTH,
+		"Player_Cone": PLAYER_CONE,
+		"SS_Air_Fear": SS_AIR_FEAR
 	}
 
 class TRIGGERS:
--- Godot Game/StickEvent.gd
+++ Godot Game/StickEvent.gd
@@ -15,7 +15,15 @@ func _process(delta):
 func _on_stick_area_entered(area):
 	Wwise.register_game_obj(self, self.get_name())
 	Wwise.set_3d_position(self, get_global_transform())
+	print(self.name)
+	#if(self.name == "TinderEvent"):
+		#playing_id = Wwise.post_event_id(AK.EVENTS.TINDER, self)
+	#if(self.name == "KindlingEvent"):
+		#playing_id = Wwise.post_event_id(AK.EVENTS.KINDLING, self)
+	#if(self.name == "FuelEvent"):
+		#playing_id = Wwise.post_event_id(AK.EVENTS.FUEL, self)
 	playing_id = Wwise.post_event_id(AK.EVENTS.FACING_STICK, self)
+	
 
 
 func _on_stick_area_exited(area):
