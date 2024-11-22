6021ebc3c773db2dd05d455cb4834d4828d5bda6
Chell-Fey99 <chell.fey99@gmail.com>
2024-11-22 14:03:25 -0500

    fixed fullscreen

--- Godot Game/campsite.gd
+++ Godot Game/campsite.gd
@@ -3,8 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#$ColorRect/Retry.hide()
-	pass
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
