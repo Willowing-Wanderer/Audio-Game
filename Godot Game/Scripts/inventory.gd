f8a68d8ab6c22602aab5a0ea737a4eb4f5597a21
Chell-Fey99 <chell.fey99@gmail.com>
2025-02-10 14:00:58 -0500

    First 4 tasks complete

--- Godot Game/Scripts/inventory.gd
+++ Godot Game/Scripts/inventory.gd
@@ -2,10 +2,32 @@ extends Node
 
 var arr = [2,3,4]
 
-var dict = {"apple" : 0,
-			"stick" : 0,
-			"druid's staff" : true}
+var items = {"Empty": null,
+			"Apple" : 0,
+			"Stick" : 0,
+			"Druid's Staff" : 1}
 			
+var selected = 0
+			
+			
+func add_item(item_name):
+	items[item_name] += 1
+	
+func play_selected_sound():
+	print(items.keys()[selected])
+	
+func scroll_up():
+	if(selected == items.size()-1):
+		selected = 0
+	else:
+		selected += 1
 
-func add(item_name):
-	print(item_name)
+func scroll_down():
+	if(selected == 0):
+		selected = items.size()-1
+	else:
+		selected -= 1
+	
+#func place(item_name):
+	#print("placing " + item_name)
+	# This is actually not how our game works at the moment, items will be placed with interactables only
