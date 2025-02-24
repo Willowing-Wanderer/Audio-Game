5a61bc1aaf582e8ff05a3d79443ac48e059d4005
willwht <wawhite@wpi.edu>
2025-02-24 05:35:03 -0500

    Changed Item_Ping.wav source file
    Changed Item_Ping.wav source file to new hum-type sound effect

--- Godot Game/GeneratedSoundBanks/wwise_ids.gd
+++ Godot Game/GeneratedSoundBanks/wwise_ids.gd
@@ -2,78 +2,78 @@ class_name AK
 
 class EVENTS:
 
-	const NARRATE_PATH = 4104917964
-	const PIXIE_GIGGLE = 848414374
-	const NARRATE_APPLE = 2194565643
-	const PICK_UP = 467838998
-	const CRYSTAL_HUM = 1127138550
-	const FACING = 54298587
-	const APPLE = 67176023
-	const HUNGRY_RACCOON = 1621431280
-	const DRUID_HELP = 876613367
-	const CRYSTAL_ITEM = 422905841
+	const DRUID_THANKS = 3109325365
 	const FOOTSTEPS = 2385628198
-	const NARRATE_DRUID = 267359535
-	const NARRATE_PIXIE = 2298942422
-	const NARRATE_CRYSTAL = 2580949965
-	const TREE_SWISH = 162560690
-	const ITEM_PING = 49143091
-	const INTERACT = 1466384055
 	const BLACK_WIND = 4020366015
-	const BACKDROP_1 = 4000258095
 	const APPLES_FALL = 1251517266
+	const DRUID_HELP = 876613367
+	const FACING = 54298587
+	const DRUID_DIALOG = 3393591398
 	const TREE_SHAKE = 3363276846
 	const PIXIE_AWW = 3300195252
-	const DRUID_DIALOG = 3393591398
-	const CLICK_FAIL = 1352158686
-	const DRUID_THANKS = 3109325365
-	const PIXIE_FLYOFF = 1075103351
-	const RACCOON_EATING = 1146653945
-	const NARRATE_NOTHING = 911107020
-	const PATH_WIND = 1008262333
-	const CRYSTAL_DROP = 1757040115
-	const DRUID_HUM = 2213069164
-	const RACCOON_THANKS = 2129084912
+	const CRYSTAL_ITEM = 422905841
 	const PIXIE1_DIALOG = 2704494418
-	const NARRATE_RACCOON = 1785858692
+	const RACCOON_THANKS = 2129084912
+	const PICK_UP = 467838998
+	const INTERACT = 1466384055
+	const PIXIE_FLYOFF = 1075103351
+	const APPLE = 67176023
+	const PIXIE_GIGGLE = 848414374
+	const CRYSTAL_DROP = 1757040115
+	const BACKDROP_1 = 4000258095
 	const NARRATE_APPLE_TREE = 3477674050
+	const CLICK_FAIL = 1352158686
+	const ITEM_PING = 49143091
+	const NARRATE_NOTHING = 911107020
+	const NARRATE_PIXIE = 2298942422
+	const TREE_SWISH = 162560690
+	const HUNGRY_RACCOON = 1621431280
+	const NARRATE_APPLE = 2194565643
+	const NARRATE_PATH = 4104917964
+	const NARRATE_DRUID = 267359535
+	const NARRATE_RACCOON = 1785858692
+	const PATH_WIND = 1008262333
+	const RACCOON_EATING = 1146653945
+	const NARRATE_CRYSTAL = 2580949965
+	const DRUID_HUM = 2213069164
+	const CRYSTAL_HUM = 1127138550
 
 	const _dict = {
-		"Narrate_Path": NARRATE_PATH,
-		"Pixie_Giggle": PIXIE_GIGGLE,
-		"Narrate_Apple": NARRATE_APPLE,
-		"Pick_Up": PICK_UP,
-		"Crystal_Hum": CRYSTAL_HUM,
-		"Facing": FACING,
-		"Apple": APPLE,
-		"Hungry_Raccoon": HUNGRY_RACCOON,
-		"Druid_Help": DRUID_HELP,
-		"Crystal_Item": CRYSTAL_ITEM,
+		"Druid_Thanks": DRUID_THANKS,
 		"Footsteps": FOOTSTEPS,
-		"Narrate_Druid": NARRATE_DRUID,
-		"Narrate_Pixie": NARRATE_PIXIE,
-		"Narrate_Crystal": NARRATE_CRYSTAL,
-		"Tree_Swish": TREE_SWISH,
-		"Item_Ping": ITEM_PING,
-		"Interact": INTERACT,
 		"Black_Wind": BLACK_WIND,
-		"Backdrop_1": BACKDROP_1,
 		"Apples_Fall": APPLES_FALL,
+		"Druid_Help": DRUID_HELP,
+		"Facing": FACING,
+		"Druid_Dialog": DRUID_DIALOG,
 		"Tree_Shake": TREE_SHAKE,
 		"Pixie_Aww": PIXIE_AWW,
-		"Druid_Dialog": DRUID_DIALOG,
-		"Click_Fail": CLICK_FAIL,
-		"Druid_Thanks": DRUID_THANKS,
-		"Pixie_Flyoff": PIXIE_FLYOFF,
-		"Raccoon_Eating": RACCOON_EATING,
-		"Narrate_Nothing": NARRATE_NOTHING,
-		"Path_Wind": PATH_WIND,
-		"Crystal_Drop": CRYSTAL_DROP,
-		"Druid_Hum": DRUID_HUM,
-		"Raccoon_Thanks": RACCOON_THANKS,
+		"Crystal_Item": CRYSTAL_ITEM,
 		"Pixie1_Dialog": PIXIE1_DIALOG,
+		"Raccoon_Thanks": RACCOON_THANKS,
+		"Pick_Up": PICK_UP,
+		"Interact": INTERACT,
+		"Pixie_Flyoff": PIXIE_FLYOFF,
+		"Apple": APPLE,
+		"Pixie_Giggle": PIXIE_GIGGLE,
+		"Crystal_Drop": CRYSTAL_DROP,
+		"Backdrop_1": BACKDROP_1,
+		"Narrate_Apple_Tree": NARRATE_APPLE_TREE,
+		"Click_Fail": CLICK_FAIL,
+		"Item_Ping": ITEM_PING,
+		"Narrate_Nothing": NARRATE_NOTHING,
+		"Narrate_Pixie": NARRATE_PIXIE,
+		"Tree_Swish": TREE_SWISH,
+		"Hungry_Raccoon": HUNGRY_RACCOON,
+		"Narrate_Apple": NARRATE_APPLE,
+		"Narrate_Path": NARRATE_PATH,
+		"Narrate_Druid": NARRATE_DRUID,
 		"Narrate_Raccoon": NARRATE_RACCOON,
-		"Narrate_Apple_Tree": NARRATE_APPLE_TREE
+		"Path_Wind": PATH_WIND,
+		"Raccoon_Eating": RACCOON_EATING,
+		"Narrate_Crystal": NARRATE_CRYSTAL,
+		"Druid_Hum": DRUID_HUM,
+		"Crystal_Hum": CRYSTAL_HUM
 	}
 
 class STATES:
@@ -86,30 +86,30 @@ class SWITCHES:
 
 class GAME_PARAMETERS:
 
-	const SS_AIR_FEAR = 1351367891
-	const SS_AIR_TURBULENCE = 4160247818
 	const SS_AIR_RPM = 822163944
 	const SS_AIR_SIZE = 3074696722
-	const PLAYER_CONE = 2039612124
-	const SS_AIR_MONTH = 2648548617
 	const SS_AIR_FURY = 1029930033
 	const SS_AIR_FREEFALL = 3002758120
-	const SS_AIR_PRESENCE = 3847924954
 	const SS_AIR_STORM = 3715662592
+	const SS_AIR_FEAR = 1351367891
+	const SS_AIR_MONTH = 2648548617
 	const SS_AIR_TIMEOFDAY = 3203397129
+	const SS_AIR_PRESENCE = 3847924954
+	const SS_AIR_TURBULENCE = 4160247818
+	const PLAYER_CONE = 2039612124
 
 	const _dict = {
-		"SS_Air_Fear": SS_AIR_FEAR,
-		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
 		"SS_Air_RPM": SS_AIR_RPM,
 		"SS_Air_Size": SS_AIR_SIZE,
-		"Player_Cone": PLAYER_CONE,
-		"SS_Air_Month": SS_AIR_MONTH,
 		"SS_Air_Fury": SS_AIR_FURY,
 		"SS_Air_Freefall": SS_AIR_FREEFALL,
-		"SS_Air_Presence": SS_AIR_PRESENCE,
 		"SS_Air_Storm": SS_AIR_STORM,
-		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY
+		"SS_Air_Fear": SS_AIR_FEAR,
+		"SS_Air_Month": SS_AIR_MONTH,
+		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY,
+		"SS_Air_Presence": SS_AIR_PRESENCE,
+		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
+		"Player_Cone": PLAYER_CONE
 	}
 
 class TRIGGERS:
