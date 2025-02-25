0ca423347f243c0ec5be3418b4037c25017928ed
Chell-Fey99 <chell.fey99@gmail.com>
2025-02-25 12:49:35 -0500

    fixed Will's stuff

--- Godot Game/GeneratedSoundBanks/wwise_ids.gd
+++ Godot Game/GeneratedSoundBanks/wwise_ids.gd
@@ -2,82 +2,82 @@ class_name AK
 
 class EVENTS:
 
-	const HOVER_DESELECT = 1523466505
-	const APPLE = 67176023
-	const FOOTSTEPS = 2385628198
-	const CRYSTAL_DROP = 1757040115
-	const NARRATE_PIXIE = 2298942422
-	const RACCOON_THANKS = 2129084912
-	const DRUID_DIALOG = 3393591398
-	const NARRATE_RACCOON = 1785858692
-	const PIXIE_GIGGLE = 848414374
-	const NARRATE_DRUID = 267359535
-	const NARRATE_PATH = 4104917964
-	const TREE_SWISH = 162560690
-	const NARRATE_NOTHING = 911107020
-	const NARRATE_APPLE = 2194565643
 	const NARRATE_CRYSTAL = 2580949965
-	const ITEM_PING = 49143091
-	const RACCOON_EATING = 1146653945
-	const HUNGRY_RACCOON = 1621431280
-	const PATH_WIND = 1008262333
-	const DRUID_HUM = 2213069164
-	const CRYSTAL_HUM = 1127138550
-	const CLICK_FAIL = 1352158686
-	const INTERACT = 1466384055
-	const FACING = 54298587
-	const PICK_UP = 467838998
-	const HOVER_SELECT = 3971078520
-	const BLACK_WIND = 4020366015
+	const NARRATE_DRUID = 267359535
+	const NARRATE_PIXIE = 2298942422
 	const NARRATE_APPLE_TREE = 3477674050
-	const BACKDROP_1 = 4000258095
+	const NARRATE_RACCOON = 1785858692
+	const ITEM_PING = 49143091
+	const NARRATE_NOTHING = 911107020
+	const NARRATE_PATH = 4104917964
+	const PATH_WIND = 1008262333
+	const TREE_SWISH = 162560690
+	const HUNGRY_RACCOON = 1621431280
+	const RACCOON_EATING = 1146653945
+	const DRUID_HUM = 2213069164
+	const NARRATE_APPLE = 2194565643
 	const CRYSTAL_ITEM = 422905841
-	const APPLES_FALL = 1251517266
 	const TREE_SHAKE = 3363276846
-	const PIXIE_FLYOFF = 1075103351
+	const CRYSTAL_HUM = 1127138550
 	const PIXIE_AWW = 3300195252
-	const PIXIE1_DIALOG = 2704494418
+	const PICK_UP = 467838998
 	const DRUID_HELP = 876613367
+	const APPLE = 67176023
+	const APPLES_FALL = 1251517266
 	const DRUID_THANKS = 3109325365
+	const HOVER_DESELECT = 1523466505
+	const HOVER_SELECT = 3971078520
+	const BACKDROP_1 = 4000258095
+	const DRUID_DIALOG = 3393591398
+	const CRYSTAL_DROP = 1757040115
+	const BLACK_WIND = 4020366015
+	const FACING = 54298587
+	const INTERACT = 1466384055
+	const CLICK_FAIL = 1352158686
+	const FOOTSTEPS = 2385628198
+	const PIXIE1_DIALOG = 2704494418
+	const RACCOON_THANKS = 2129084912
+	const PIXIE_FLYOFF = 1075103351
+	const PIXIE_GIGGLE = 848414374
 
 	const _dict = {
-		"Hover_Deselect": HOVER_DESELECT,
-		"Apple": APPLE,
-		"Footsteps": FOOTSTEPS,
-		"Crystal_Drop": CRYSTAL_DROP,
-		"Narrate_Pixie": NARRATE_PIXIE,
-		"Raccoon_Thanks": RACCOON_THANKS,
-		"Druid_Dialog": DRUID_DIALOG,
-		"Narrate_Raccoon": NARRATE_RACCOON,
-		"Pixie_Giggle": PIXIE_GIGGLE,
-		"Narrate_Druid": NARRATE_DRUID,
-		"Narrate_Path": NARRATE_PATH,
-		"Tree_Swish": TREE_SWISH,
-		"Narrate_Nothing": NARRATE_NOTHING,
-		"Narrate_Apple": NARRATE_APPLE,
 		"Narrate_Crystal": NARRATE_CRYSTAL,
-		"Item_Ping": ITEM_PING,
-		"Raccoon_Eating": RACCOON_EATING,
-		"Hungry_Raccoon": HUNGRY_RACCOON,
-		"Path_Wind": PATH_WIND,
-		"Druid_Hum": DRUID_HUM,
-		"Crystal_Hum": CRYSTAL_HUM,
-		"Click_Fail": CLICK_FAIL,
-		"Interact": INTERACT,
-		"Facing": FACING,
-		"Pick_Up": PICK_UP,
-		"Hover_Select": HOVER_SELECT,
-		"Black_Wind": BLACK_WIND,
+		"Narrate_Druid": NARRATE_DRUID,
+		"Narrate_Pixie": NARRATE_PIXIE,
 		"Narrate_Apple_Tree": NARRATE_APPLE_TREE,
-		"Backdrop_1": BACKDROP_1,
+		"Narrate_Raccoon": NARRATE_RACCOON,
+		"Item_Ping": ITEM_PING,
+		"Narrate_Nothing": NARRATE_NOTHING,
+		"Narrate_Path": NARRATE_PATH,
+		"Path_Wind": PATH_WIND,
+		"Tree_Swish": TREE_SWISH,
+		"Hungry_Raccoon": HUNGRY_RACCOON,
+		"Raccoon_Eating": RACCOON_EATING,
+		"Druid_Hum": DRUID_HUM,
+		"Narrate_Apple": NARRATE_APPLE,
 		"Crystal_Item": CRYSTAL_ITEM,
-		"Apples_Fall": APPLES_FALL,
 		"Tree_Shake": TREE_SHAKE,
-		"Pixie_Flyoff": PIXIE_FLYOFF,
+		"Crystal_Hum": CRYSTAL_HUM,
 		"Pixie_Aww": PIXIE_AWW,
-		"Pixie1_Dialog": PIXIE1_DIALOG,
+		"Pick_Up": PICK_UP,
 		"Druid_Help": DRUID_HELP,
-		"Druid_Thanks": DRUID_THANKS
+		"Apple": APPLE,
+		"Apples_Fall": APPLES_FALL,
+		"Druid_Thanks": DRUID_THANKS,
+		"Hover_Deselect": HOVER_DESELECT,
+		"Hover_Select": HOVER_SELECT,
+		"Backdrop_1": BACKDROP_1,
+		"Druid_Dialog": DRUID_DIALOG,
+		"Crystal_Drop": CRYSTAL_DROP,
+		"Black_Wind": BLACK_WIND,
+		"Facing": FACING,
+		"Interact": INTERACT,
+		"Click_Fail": CLICK_FAIL,
+		"Footsteps": FOOTSTEPS,
+		"Pixie1_Dialog": PIXIE1_DIALOG,
+		"Raccoon_Thanks": RACCOON_THANKS,
+		"Pixie_Flyoff": PIXIE_FLYOFF,
+		"Pixie_Giggle": PIXIE_GIGGLE
 	}
 
 class STATES:
@@ -90,30 +90,30 @@ class SWITCHES:
 
 class GAME_PARAMETERS:
 
-	const PLAYER_CONE = 2039612124
+	const SS_AIR_FEAR = 1351367891
+	const SS_AIR_TURBULENCE = 4160247818
+	const SS_AIR_RPM = 822163944
 	const SS_AIR_SIZE = 3074696722
+	const PLAYER_CONE = 2039612124
+	const SS_AIR_MONTH = 2648548617
+	const SS_AIR_FURY = 1029930033
+	const SS_AIR_FREEFALL = 3002758120
 	const SS_AIR_PRESENCE = 3847924954
 	const SS_AIR_STORM = 3715662592
 	const SS_AIR_TIMEOFDAY = 3203397129
-	const SS_AIR_RPM = 822163944
-	const SS_AIR_FEAR = 1351367891
-	const SS_AIR_MONTH = 2648548617
-	const SS_AIR_FREEFALL = 3002758120
-	const SS_AIR_FURY = 1029930033
-	const SS_AIR_TURBULENCE = 4160247818
 
 	const _dict = {
-		"Player_Cone": PLAYER_CONE,
+		"SS_Air_Fear": SS_AIR_FEAR,
+		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
+		"SS_Air_RPM": SS_AIR_RPM,
 		"SS_Air_Size": SS_AIR_SIZE,
+		"Player_Cone": PLAYER_CONE,
+		"SS_Air_Month": SS_AIR_MONTH,
+		"SS_Air_Fury": SS_AIR_FURY,
+		"SS_Air_Freefall": SS_AIR_FREEFALL,
 		"SS_Air_Presence": SS_AIR_PRESENCE,
 		"SS_Air_Storm": SS_AIR_STORM,
-		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY,
-		"SS_Air_RPM": SS_AIR_RPM,
-		"SS_Air_Fear": SS_AIR_FEAR,
-		"SS_Air_Month": SS_AIR_MONTH,
-		"SS_Air_Freefall": SS_AIR_FREEFALL,
-		"SS_Air_Fury": SS_AIR_FURY,
-		"SS_Air_Turbulence": SS_AIR_TURBULENCE
+		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY
 	}
 
 class TRIGGERS:
@@ -144,12 +144,12 @@ class AUX_BUSSES:
 
 class AUDIO_DEVICES:
 
-	const NO_OUTPUT = 2317455096
 	const SYSTEM = 3859886410
+	const NO_OUTPUT = 2317455096
 
 	const _dict = {
-		"No_Output": NO_OUTPUT,
-		"System": SYSTEM
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
 	}
 
 class EXTERNAL_SOURCES:
