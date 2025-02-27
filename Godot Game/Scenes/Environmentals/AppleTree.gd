85f3964ee1cbdfbc74c6df8d81347242abab6b62
Chell-Fey99 <chell.fey99@gmail.com>
2025-02-27 10:31:50 -0500

    tree now only gives apples once

--- Godot Game/Scenes/Environmentals/AppleTree.gd
+++ Godot Game/Scenes/Environmentals/AppleTree.gd
@@ -11,6 +11,8 @@ extends Area3D
 
 var apple_scene = preload("res://Scenes/Items/apple.tscn")
 
+var has_apples = true
+
 func _ready():
 	Wwise.register_game_obj(self, self.get_name())
 	Wwise.set_3d_position(self, get_global_transform())
@@ -23,22 +25,24 @@ func on_click(selected):
 	Wwise.post_event_id(AK.EVENTS.INTERACT, player)
 	tree_shake.post_event()
 	await get_tree().create_timer(3).timeout
-	apples_fall.post_event()
-	# Could refactor this to make the sounds of the apples happen where the apples actually fall
 	
-	var apple1 = apple_scene.instantiate()
-	var apple2 = apple_scene.instantiate()
-	var apple3 = apple_scene.instantiate()
-	apple1.rotation = Vector3(0,deg_to_rad(20),0)
-	apple1.player = player
-	apple1.object_name = "Apple"
-	apple2.rotation = Vector3(0,deg_to_rad(-50),0)
-	apple2.player = player
-	apple2.object_name = "Apple"
-	apple3.rotation = Vector3(0,deg_to_rad(150),0)
-	apple3.player = player
-	apple3.object_name = "Apple"
-
-	circle.add_child(apple1)
-	circle.add_child(apple2)
-	circle.add_child(apple3)
+	if(has_apples):
+		apples_fall.post_event()
+		var apple1 = apple_scene.instantiate()
+		var apple2 = apple_scene.instantiate()
+		var apple3 = apple_scene.instantiate()
+		apple1.rotation = Vector3(0,deg_to_rad(20),0)
+		apple1.player = player
+		apple1.object_name = "Apple"
+		apple2.rotation = Vector3(0,deg_to_rad(-50),0)
+		apple2.player = player
+		apple2.object_name = "Apple"
+		apple3.rotation = Vector3(0,deg_to_rad(150),0)
+		apple3.player = player
+		apple3.object_name = "Apple"
+		circle.add_child(apple1)
+		circle.add_child(apple2)
+		circle.add_child(apple3)
+		
+		has_apples = false
+		
