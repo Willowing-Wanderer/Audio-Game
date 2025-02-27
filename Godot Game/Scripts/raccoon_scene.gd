56c7798a9d000760ec23827d1dd25ed23098324b
Chell-Fey99 <chell.fey99@gmail.com>
2025-02-27 15:46:13 -0500

    handing it back

--- Godot Game/Scripts/raccoon_scene.gd
+++ Godot Game/Scripts/raccoon_scene.gd
@@ -2,10 +2,13 @@ extends Node3D
 
 @export var narrate_controls:AkEvent3D
 @export var narrate_ending:AkEvent3D
+@export var narrate_start:AkEvent3D
 @export var forest_ambience:AkEvent3D
 
 func _ready():
 	$Player.set_cutscene(true)
+	narrate_start.post_event()
+	await get_tree().create_timer(20).timeout
 	narrate_controls.post_event()
 	await get_tree().create_timer(9).timeout
 	$Player.set_cutscene(false)
