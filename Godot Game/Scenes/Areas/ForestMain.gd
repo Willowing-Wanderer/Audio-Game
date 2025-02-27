2ac38688163df99b06ca1277d42b741689b896b4
Chell-Fey99 <chell.fey99@gmail.com>
2025-02-27 12:15:34 -0500

    Figured out how to load multiple scenes

--- Godot Game/Scenes/Areas/ForestMain.gd
+++ Godot Game/Scenes/Areas/ForestMain.gd
@@ -1,10 +1,14 @@
 extends Node3D
 
+var title_screen = preload("res://Scenes/UI/title_screen.tscn")
+var raccoon_scene = preload("res://Scenes/Areas/raccoon_scene.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	add_child(title_screen.instantiate())
 
+func start_game():
+	add_child(raccoon_scene.instantiate())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- Godot Game/Scripts/TitleScreen.gd
+++ Godot Game/Scripts/TitleScreen.gd
@@ -1,8 +1,10 @@
 extends ColorRect
 
+signal start_game
 
 func _input(event):
 	if(event is InputEventMouseButton):
 		# Left click
 		if (event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT):
-			pass
+			get_parent().start_game()
+			queue_free()
