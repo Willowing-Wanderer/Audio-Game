c4770ea0d56a68c4a3d294a5a8998fe629a351be
Chell-Fey99 <chell.fey99@gmail.com>
2025-03-04 17:04:44 -0500

    Made cave map, but getting an error when using FoyerPath

--- Godot Game/Scripts/ForestMain.gd
+++ Godot Game/Scripts/ForestMain.gd
@@ -2,13 +2,15 @@ extends Node3D
 
 var title_screen = preload("res://Scenes/UI/title_screen.tscn")
 var raccoon_scene = preload("res://Scenes/Areas/raccoon_scene.tscn")
+var cave_scene = preload("res://Scenes/Areas/CaveScene.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_child(title_screen.instantiate())
 
 func start_game():
-	add_child(raccoon_scene.instantiate())
+	#add_child(raccoon_scene.instantiate())
+	add_child(cave_scene.instantiate())
 
 func finish_game():
 	add_child(title_screen.instantiate())
