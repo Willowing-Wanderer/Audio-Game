2dc0eb40b420431e4871da06351d68af8c9a6101
Willowing-Wanderer <chell.fey99@gmail.com>
2025-04-04 15:17:43 -0400

    title screen works now too

--- Godot Game/Scripts/ForestMain.gd
+++ Godot Game/Scripts/ForestMain.gd
@@ -1,16 +1,17 @@
 extends Node3D
 
-var title_screen = preload("res://Scenes/UI/title_screen.tscn")
+var title_screen
+var title_menu = preload("res://Scenes/UI/TitleMenu.tscn")
 var raccoon_scene = preload("res://Scenes/Areas/raccoon_scene.tscn")
 var cave_scene = preload("res://Scenes/Areas/CaveScene.tscn")
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#add_child(title_screen.instantiate())
-	start_game()
+	title_screen = title_menu.instantiate()
+	add_child(title_screen)
 
 func start_game():
+	remove_child(title_screen)
 	var raccoon = raccoon_scene.instantiate()
 	raccoon.player = $Player
 	add_child(raccoon)
