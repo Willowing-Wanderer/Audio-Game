04734186c08883fb9577ba4639dba0e8ab853a50
Willowing-Wanderer <chell.fey99@gmail.com>
2025-04-10 11:52:22 -0400

    Made sure everything was assigned in Wwise

--- Godot Game/Scripts/ForestMain.gd
+++ Godot Game/Scripts/ForestMain.gd
@@ -14,9 +14,10 @@ func _ready():
 
 func start_game():
 	remove_child(title_screen)
-	var raccoon = raccoon_scene.instantiate()
-	raccoon.player = $Player
-	add_child(raccoon)
+	finish_level_1()
+	#var raccoon = raccoon_scene.instantiate()
+	#raccoon.player = $Player
+	#add_child(raccoon)
 
 func finish_level_1():
 	await get_tree().create_timer(0.1).timeout
