43833a2127c59a4a827060a87a8891805e62982a
Willowing-Wanderer <chell.fey99@gmail.com>
2025-04-11 20:09:58 -0400

    updated mother bear and mushroom patch

--- Godot Game/Scenes/Characters/MushroomPatch.gd
+++ Godot Game/Scenes/Characters/MushroomPatch.gd
@@ -35,7 +35,7 @@ func _ready():
 
 func on_click(selected):
 	player.set_cutscene(true)
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(0.1).timeout
 	mushroom_pick.post_event()
 
 func _on_mushroom_pick_end_of_event(data):
